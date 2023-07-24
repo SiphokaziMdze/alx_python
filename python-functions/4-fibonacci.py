c50bad7237a864cf19bbbc9fce5a922389d29882
Siphokazi Mdze <veronicamdze@gmail.com>
2023-07-24 13:59:45 -0700

    Task 4

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-fibonacci_sequence = __import__('4-fibonacci').fibonacci_sequence
 def fibonacci_sequence(n):
     if n <= 0:
         return []
+
     fibonacci_numbers = [0, 1]
     while len(fibonacci_numbers) < n:
         next_number = fibonacci_numbers[-1] + fibonacci_numbers[-2]
         fibonacci_numbers.append(next_number)
-         return fibonacci_numbers
-     
+
+    return fibonacci_numbers 
