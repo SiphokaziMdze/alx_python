f9ab439439e2d1d15a0538740048dfb17aced91f
Siphokazi Mdze <veronicamdze@gmail.com>
2023-07-24 13:55:08 -0700

    Task 5

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for number in range(99):
-    print("{:02d}".format(number), end=", " if number < 99 else "\n")
+for num in range(100):
+    print("{:02d}".format(num), end=", " if num != 99 else "\n")
+
