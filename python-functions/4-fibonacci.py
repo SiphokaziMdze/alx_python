d5098377cc8fe27637e9bbfee9f365b916d8c674
Siphokazi Mdze <veronicamdze@gmail.com>
2023-07-24 13:34:53 -0700

    Task 4

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
-def reverse_string(string):
-    return string[::-1]
-
+def fibonacci_sequence(n):
+    if n <= 0:
+        return []
+    fibonacci_numbers = [0, 1]
+    while len(fibonacci_numbers) < n:
+        next_number = fibonacci_numbers[-1] + fibonacci_numbers[-2]
+        fibonacci_numbers.append(next_number)
+         return fibonacci_numbers
+     print(fibonacci_sequence(6))
+     print(fibonacci_sequence(1))
+     print(fibonacci_sequence(0))
+     print(fibonacci_sequence(20))
