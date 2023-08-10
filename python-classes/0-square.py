8437a3565ad3ed8ffd69474e3651ff2664d5fc09
Siphokazi Mdze <veronicamdze@gmail.com>
2023-08-09 18:39:43 -0700

    Task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -3,22 +3,8 @@ Square = __import__('0-square').Square
 
 class Square:
     
-    def __init__(self, size)
+    def __init__(self, size):
 
         self.__size = size
 
-if __name__ == "__main__":
-    my_square = Square(3)
-    print(type(my_square))
-    print(my_square.__dict__)
-
-    try:
-        print(my_square.size)
-    except Exception as e:
-        print(e)
-
-    try:
-        print(my_square.__size)
-    except Exception as e:
-        print(e)
 
