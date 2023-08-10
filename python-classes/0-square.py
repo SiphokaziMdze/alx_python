3228fe7a2c2a72872f40a46733847dc8f066d3c5
Siphokazi Mdze <veronicamdze@gmail.com>
2023-08-09 18:53:50 -0700

    Task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -4,11 +4,18 @@ Square = __import__('0-square').Square
 class Square:
     """
     This class represents a square.
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(self, size): Initializes a new Square instance with the     given size.
     """
     
     def __init__(self, size):
         """
         Initializes a Square instance.
+        Args:
+            size (int): The size of the square.
         """
 
         self.__size = size
