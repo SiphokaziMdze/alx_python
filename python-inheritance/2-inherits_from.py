02c7cf275523abcc1842cbd6683e1647ed3a92b6
Siphokazi Mdze <veronicamdze@gmail.com>
2023-08-09 19:33:03 -0700

    Task 2&3

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,3 +1,26 @@
+#!/usr/bin/python3
+
+"""
+2-inherits_from module
+
+This module contains a function that checks if an object is an instance of a class that inherited (directly or indirectly)
+from a specified class.
+"""
+
 def inherits_from(obj, a_class):
+
+    """
+    Check if an object is an instance of a class that inherited (directly or indirectly)
+    from the specified class.
+
+    Args:
+        obj: The object to be checked.
+        a_class: The class to compare against.
+
+    Returns:
+        True if the object is an instance of a class that inherited from the specified class,
+        otherwise False.
+    """
+
     return isinstance(obj, a_class) and type(obj) is not a_class
 
--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
+
 class BaseGeometry:
+
+    """
+    An empty base class for geometry-related classes.
+    """
+
     pass
 
