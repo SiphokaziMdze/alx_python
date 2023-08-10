370d0d448620d8cb64dd75b5b7a34a4dcb80e15f
Siphokazi Mdze <veronicamdze@gmail.com>
2023-08-09 20:38:41 -0700

    Task 6

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,5 +1,33 @@
+#!/usr/bin/python3
+
+"""
+6-rectangle module
+
+This module defines the Rectangle class that inherits from BaseGeometry. It includes methods for calculating
+area and validating integer values for width and height.
+"""
+
+class BaseGeometry:
+
+    """
+    A base class for geometry-related classes.
+    """
+
 class Rectangle(BaseGeometry):
+    """
+    A class representing a rectangle, inheriting from BaseGeometry.
+    """
+
     def __init__(self, width, height):
+
+        """
+        Initialize a Rectangle instance with width and height.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
